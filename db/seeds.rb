5e12c2dea63d0fd053d0b958fed243a9cb2dc436
Tong Dang Tinh <tinhtongdang@gmail.com>
2020-04-11 00:18:44 +0700

    Add models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+25.times do |i|
+  Product.create!(
+    name: 'Pizza',
+    description: 'Lorem ipsum bbq chicken upstate newyork free range dough',
+    price: 11.99,
+    catering: true
+  )
+end
